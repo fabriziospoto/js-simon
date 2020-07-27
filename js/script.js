63783c0d9bf1594c37ba9a35cf41d68edd2a1593
Fabrizio <fabriziospoto1@gmail.com>
2020-07-27 16:41:42 +0200

    Init
    Init

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Un alert espone 5 numeri casuali.
+// Dopo la chiusura (manuale, cioè facendo click su ok) dell'alert, parte un timer di 30 secondi.
+// Dopo i 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e
+// quali dei numeri da indovinare sono stati individuati.
